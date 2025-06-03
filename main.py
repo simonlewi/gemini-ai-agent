ba26ad00c200a5dbc328b8bac4b4bfecd6b79c02
simonlewi <simon.wilen@gmail.com>
2025-06-03 19:40:21 +0200

    first commit innit

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+
+def main():
+    load_dotenv()
+
+    args = sys.argv[1:]
+
+    if not args:
+        print("AI Code Assistant:")
+        print('\nUsage: python main.py "Your prompt here"')
+        print('Example: python main.py "What is a krusty krab?"')
+        sys.exit(1)
+    user_prompt = " ".join(args)
+      
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001", 
+        contents=user_prompt,
+    )
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
+    print("Response:")
+    print(response.text)
+
+if __name__ == "__main__":
+    main()
