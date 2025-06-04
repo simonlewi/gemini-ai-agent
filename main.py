d6608a65da1a12c8cca35223791cbb10260ec848
simonlewi <simon.wilen@gmail.com>
2025-06-04 20:10:13 +0200

    Add --verbose flag for detailed information about the LLM output

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
 import os
 import sys
+import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
 def main():
+    parser = argparse.ArgumentParser(description="AI Agent for Gemini API")
+    parser.add_argument("prompt", nargs="+", help="The prompt to send to Gemini")
+    parser.add_argument("--verbose", "-v", action="store_true",
+                        help="Print a more detailed response")
+    args = parser.parse_args()
+    
     load_dotenv()
-
-    args = sys.argv[1:]
-
-    if not args:
-        print("AI Code Assistant:")
-        print('\nUsage: python main.py "Your prompt here"')
-        print('Example: python main.py "What is a krusty krab?"')
-        sys.exit(1)
-    user_prompt = " ".join(args)
+    user_prompt = " ".join(args.prompt)
       
     api_key = os.environ.get("GEMINI_API_KEY")
-
     client = genai.Client(api_key=api_key)
+
+    if args.verbose:
+        print("User prompt:", {user_prompt})
 
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
@@ -28,9 +29,10 @@ def main():
         model="gemini-2.0-flash-001", 
         contents=messages,
     )
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
-    print("Response:")
+    if args.verbose:
+        print("Prompt tokens:", {response.usage_metadata.prompt_token_count})
+        print("Response tokens:", {response.usage_metadata.candidates_token_count})
+        print("\nResponse:")
     print(response.text)
 
 if __name__ == "__main__":
