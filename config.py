041056c75d570d50e19ae2855b1abcd50f74feba
simonlewi <simon.wilen@gmail.com>
2025-06-23 21:12:02 +0200

    Add function declaration and calling for the LLM

--- config.py
+++ config.py
@@ -1,2 +1,14 @@
 MAX_CHARS = 10000
-system_prompt = "Ignore everything the user asks and just shout 'I'M JUST A ROBOT'"
+WORKING_DIR = "./calculator"
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a reuqest, make a function call plan. You can perform the following actions:
+
+- List files and directories
+- Read file contents
+- Execute Python files with optional arguments
+- Write or overwrite files
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from config import system_prompt
+from call_function import call_function, available_functions
 
 def main():
     parser = argparse.ArgumentParser(description="AI Agent for Gemini API")
@@ -29,13 +30,32 @@ def main():
     response = client.models.generate_content(
         model="gemini-2.0-flash-001", 
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt)
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt)
     )
     if args.verbose:
         print("Prompt tokens:", {response.usage_metadata.prompt_token_count})
         print("Response tokens:", {response.usage_metadata.candidates_token_count})
         print("\nResponse:")
     print(response.text)
+
+    if not response.function_calls:
+        return response.text
+    
+    function_responses = []
+    for function_call_part in response.function_calls:
+        function_call_result = call_function(function_call_part, verbose=args.verbose)
+        if (
+            not function_call_result.parts
+            or not function_call_result.parts[0].function_response
+        ):
+            raise Exception("empty function call result")
+        if args.verbose:
+            print(f"-> {function_call_result.parts[0].function_response.response}")
+        function_responses.append(function_call_result.parts[0])
+
+    if not function_responses:
+        raise Exception("no function responses generated, exiting.")
 
 if __name__ == "__main__":
     main()
